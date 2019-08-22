1aa407920ba8e80169928d7c85559e8d1d6f9b39
kuromatia <yuki.yserver@gmail.com>
2019-08-22 16:23:11 +0900

    test set_datetime

--- script/write_datetime.py
+++ script/write_datetime.py
@@ -8,7 +8,7 @@ import subprocess as sp
 
 def main():
     today = datetime.datetime.today().strftime("%Y/%m/%d %H:%M:%S")
-    print(today)
+    sp.call("date -s {0}".format(today), shell=True)
 
 
 
