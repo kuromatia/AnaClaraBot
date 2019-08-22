9ff822d29c4777050a2e62ae974b3254e8e74161
kuromatia <yuki.yserver@gmail.com>
2019-08-22 16:28:50 +0900

    test set_datetime

--- script/write_datetime.py
+++ script/write_datetime.py
@@ -8,7 +8,7 @@ import subprocess as sp
 
 def main():
     today = datetime.datetime.today().strftime("%Y/%m/%d %H:%M:%S")
-    sp.call("date -s \"{0}\"".format(today), shell=True)
+    sp.call("sudo date -s \"{0}\"".format(today), shell=True)
 
 if __name__ == '__main__':
     main()
