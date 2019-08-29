a891f1b86052c9ff3c2cb452bab4f7cdf1f9a130
kuromatia <yuki.yserver@gmail.com>
2019-08-29 19:15:17 +0900

    r

--- runner_navi.py
+++ runner_navi.py
@@ -9,9 +9,11 @@ def main():
     tenki_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)),"patch/tenki/tenki.txt")
 
     speak0_py = os.path.join(os.path.dirname(os.path.abspath(__file__)),"speak0.py")
+    wav_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"*.wav")
 
     sp.call("python3 {0}".format(get_tenki), shell=True)
     sp.call("python3 {0} -i {1}".format(speak0_py, tenki_txt), shell=True)
+    sp.call("rm {0}".format(wav_path), shell=True)
 
 
 if __name__ == '__main__':
