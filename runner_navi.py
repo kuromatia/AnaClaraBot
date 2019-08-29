949567e93bf10979f44a95f8ccb7a267ad826b5d
kuromatia <yuki.yserver@gmail.com>
2019-08-29 18:36:50 +0900

    renew2

--- runner_navi.py
+++ runner_navi.py
@@ -8,8 +8,10 @@ def main():
     get_tenki = os.path.join(os.path.dirname(os.path.abspath(__file__)),"patch/tenki/get_tenki.py")
     tenki_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)),"patch/tenki/tenki.txt")
 
+    speak0_py = s.path.join(os.path.dirname(os.path.abspath(__file__)),"speak0.py")
+
     sp.call("python3 {0}".format(get_tenki), shell=True)
-    sp.call("python3 speak0.py -i \"{0}\"".format(tenki_txt), shell=True)
+    sp.call("python3 {0} -i \"{1}\"".format(speak0_py, tenki_txt), shell=True)
 
 
 if __name__ == '__main__':
